3216aeeeb11ac2e0a9152d8e6485a7be3cef4c65
nourhan36 <nourhanibrahim2004@gmail.com>
2024-01-20 20:48:16 +0200

    Initial commit

--- app/src/main/java/com/example/islamiapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/islamiapp/ui/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.islamiapp
+package com.example.islamiapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.islamiapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
