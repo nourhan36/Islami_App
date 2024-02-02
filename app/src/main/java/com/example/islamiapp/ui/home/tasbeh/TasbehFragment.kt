77cc14c24f3a7b9bd58d110fa4825d188fe83104
nourhan36 <nourhanibrahim2004@gmail.com>
2024-02-02 15:49:37 +0200

    Initial commit

--- app/src/main/java/com/example/islamiapp/ui/home/tasbeh/TasbehFragment.kt
+++ app/src/main/java/com/example/islamiapp/ui/home/tasbeh/TasbehFragment.kt
@@ -10,6 +10,8 @@ import com.example.islamiapp.databinding.FragmentTasbehBinding
 class TasbehFragment : Fragment() {
 
     lateinit var viewBinding: FragmentTasbehBinding
+    var dhikrPosition = 0
+    var counter = 0
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,5 +19,32 @@ class TasbehFragment : Fragment() {
     ): View? {
         viewBinding = FragmentTasbehBinding.inflate(inflater, container, false)
         return viewBinding.root
+    }
+
+    private val dhikr = listOf(
+        "سبحان الله",
+        "الحمدلله",
+        "الله اكبر"
+    )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.dhikrTv.text = dhikr[dhikrPosition]
+        onSebhaClick()
+    }
+
+    private fun onSebhaClick() {
+        viewBinding.sebhaBodyImg.setOnClickListener {
+            viewBinding.sebhaBodyImg.rotation += (360 / 33).toFloat()
+            if (counter < 33)
+                counter++
+            else {
+                counter = 0
+                dhikrPosition =
+                    if (dhikrPosition < dhikr.size - 1) ++dhikrPosition else 0
+                viewBinding.dhikrTv.text = dhikr[dhikrPosition]
+            }
+            viewBinding.counterTv.text = counter.toString()
+        }
     }
 }
